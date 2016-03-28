7ef2f12ee07f747b56a4fe526b97ab2c0c1bf125
john <johnsonyuehit@163.com>
2016-03-28 16:02:23 +0800

    3.28

---
+++ deployment/purge.sh
@@ -0,0 +1,2 @@
+docker stop shipyard-controller shipyard-swarm-agent  shipyard-swarm-manager shipyard-discovery shipyard-rethinkdb
+docker rm shipyard-controller shipyard-swarm-agent  shipyard-swarm-manager shipyard-discovery shipyard-rethinkdb
